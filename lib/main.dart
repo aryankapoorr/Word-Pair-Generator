6a0d67bd215c3128c39da5189cf00e3bf0ef9b3e
aryankapoorr <aryan.kapoor.1102@gmail.com>
2022-07-10 20:30:00 -0500

    intial static page

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,50 @@ class MyApp extends StatelessWidget {
     final wordPair = WordPair.random();
 
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: Scaffold(
-            appBar: AppBar(title: Text('Word Pair Generator')),
-            body: Center(
-              child: Text(wordPair.asPascalCase),
-            )));
+        theme: ThemeData(primarySwatch: Colors.purple), home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 18),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Pair Generator'),
+      ),
+      body: _buildList(),
+    );
   }
 }
